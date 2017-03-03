e78990b11c728cab86281f3f3156c8fbb22db11d
hanketgithub <hanketgoog@gmail.com>
2017-03-04 02:59:00 +0800

    Update main.c
    rindex to strrchr

--- main.c
+++ main.c
@@ -80,7 +80,7 @@ int main(int argc, const char * argv[]) {
     }
     
     // specify output file name
-    cp = rindex(argv[1], '.');
+    cp = strrchr(argv[1], '.');
     strncpy(output.name, argv[1], cp - argv[1]);
     strcat(output.name, "_nv12");
     strcat(output.name, cp);
